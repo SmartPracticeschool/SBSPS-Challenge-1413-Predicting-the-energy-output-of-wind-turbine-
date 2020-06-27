451f5f559ce65b0ba545d419c58df416e57f935d
anantdevcs <anantdeviitism@gmail.com>
2020-06-27 17:19:25 +0530

    Some Refactoring

--- application.py
+++ application.py
@@ -10,7 +10,6 @@ from weatherAPIWrapper import getPredictions
 import requests
 from timerfunctions import update_iamkey
 import threading
-from script import update
 import pickle
 app = Flask(__name__, static_url_path='')
 
@@ -47,55 +46,26 @@ elif os.path.isfile('vcap-local.json'):
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
 
-threading.Timer(60*15, update_iamkey).start()
+#update the iamkey requred for IBM Watson API every 15min = 15 * 60
+# threading.Timer(60*15, update_iamkey).start()
 
 
 @app.route('/')
 def root():
+    #Home Page showing all options for the user
     return render_template('index.html')
 
-# /* Endpoint to greet and add a new visitor to database.
-# * Send a POST request to localhost:8000/api/visitors with body
-# * {
-# *     "name": "Bob"
-# * }
-# */
-@app.route('/api/visitors', methods=['GET'])
-def get_visitor():
-    if client:
-        return jsonify(list(map(lambda doc: doc['name'], db)))
-    else:
-        print('No database')
-        return jsonify([])
-
-
-@app.route('/api/visitors', methods=['POST'])
-def put_visitor():
-    user = request.json['name']
-    data = {'name':user}
-    if client:
-        my_document = db.create_document(data)
-        data['_id'] = my_document['_id']
-        return jsonify(data)
-    else:
-        print('No database')
-        return jsonify(data)
-
-
-# @app.route('/get_started')
-# def get_started():
 
 @app.route('/predict')
 def predict():
+    #Returns the page for prediction as per city
     return render_template('predict.html')
     
 @app.route('/predict/all_countries')
 def all_countries():
+    #API --> outputs all counties in a json format
     countries = []
-    # file = json.load(open('names.json', 'r'))
-    # for country_name in file.values():
-    #     countries.append(country_name)
-    
+    #countries.pkl is pickled version of a list of all countries
     f = open('countries.pkl', 'rb')
     countries = pickle.load(f)
 
@@ -105,63 +75,87 @@ def all_countries():
 
 @app.route('/predict/cities/<country>')
 def cities(country):
+    update_iamkey()
+    #API for delivering a list of all cities in a country using GET
     all_cities = []
-    # for city in os.listdir("./country_data/" + country):
-    #     with open("./country_data/" + country + "/" + city) as f:
-    #         all_cities.append(json.load(f))
-    # cur = {"cities" : all_cities}
-
+    #data is a pickled version of a dict of country name TO list of city names
     f = open('data', 'rb')
     city_in_country = pickle.load(f)
-    # print(city_in_country[country])
     for entry in city_in_country[country]:
         all_cities.append(entry[0]) 
 
     cur = {"cities" : all_cities}
+    f.close()
     return jsonify(cur)
 
 @app.route('/predict/submit', methods = ['POST'])
 def submit():
-    print(request.form)
+    #API to return a predictions of expected powers 
     city = request.form['city'] 
-    resp, initial_time = getPredictions(city)
-    
-    return jsonify({
-        'powers' : resp,
-        'time' : initial_time
-    })
-    
+    forbidden = ['ā', 'ē', 'ī', "ō", 'ū', 'ȳ', 'á', 'é', 'í' , 'ó', 'ú', 'ý', 'ï']
+    allowed = ['a', 'e', 'i', 'o', 'u','y', 'a', 'e', 'i', 'o', 'u','y' , 'i' ]
+    for i in range(len(forbidden)):
+        city = city.replace(forbidden[i], allowed[i])
+    try:
+        resp, initial_time = getPredictions(city)
+        return jsonify({
+            'powers' : resp,
+            'time' : initial_time
+        })
+    except Exception as e:
+        return str(e)
 
-@app.route('/result/<city>')
-def result(city):
-    predictions = getPredictions(city)[0]
-    return jsonify(predictions)
+        
+    
+##########DEBUG PURPOSES ONLY ###########
+# @app.route('/result/<city>')
+# def result(city):
+#     predictions = getPredictions(city)[0]
+#     return jsonify(predictions)
+
+#########################################
 
 
 @app.route('/get_windspeeds', methods = ['POST'])
 def get_windspeeds():
+    #API to return a json of expected windspeeds
+
     city = request.form['city']
+    forbidden = ['ā', 'ē', 'ī', "ō", 'ū', 'ȳ', 'á', 'é', 'í' , 'ó', 'ú', 'ý', 'ï']
+    allowed = ['a', 'e', 'i', 'o', 'u','y', 'a', 'e', 'i', 'o', 'u','y' , 'i' ]
+    for i in range(len(forbidden)):
+        city = city.replace(forbidden[i], allowed[i])
     weather_api = "54e3f61b512d6ae30ebd81acd43d155c"
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_api}"
-    resp = requests.post(url).json()
-    
-    windspeeds  = []
-    for entry in resp['list'] :
-        windspeeds.append(float(entry['wind']['speed']))
-    return jsonify({
-        'windspeeds' : windspeeds
-    })
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_api}"
+        resp = requests.post(url).json()
+        
+        windspeeds  = []
+        for entry in resp['list'] :
+            windspeeds.append(float(entry['wind']['speed']))
+        return jsonify({
+            'windspeeds' : windspeeds
+        })
+    except :
+        return jsonify({
+            'error' : 'Çan not access the openweather maps'
+        })
+
 
 
 @app.route('/compare')
 def compare():
+    #Returns a webpage for compare route
     return render_template('compare.html')
 
-@app.route('/test/<ws>/<wd>')
-def test(ws, wd):
-    return str(power_pred(int(ws), int(wd)))
+##########DEBUG PURPOSES ONLY ###########
+# @app.route('/test/<ws>/<wd>')
+# def test(ws, wd):
+#     return str(power_pred(int(ws), int(wd)))
+#########################################
 @app.route('/down')
 def down():
+    #this funtion will download pickled data requred by the web app from Google Drive 
     from google_drive_downloader import GoogleDriveDownloader as gdd
     gdd.download_file_from_google_drive(file_id='1K2obxXAwzg63KlRAlBkuIxB79YCYq9lP',dest_path='./data')
     f = open('data', 'rb')
@@ -176,8 +170,6 @@ def shutdown():
         client.disconnect()
 
 if __name__ == '__main__':
-    update_iamkey()
     
-    threading.Timer(1200, update_iamkey).start()
     app.run(host='0.0.0.0', port=port, debug=True)
     
--- mlPredictorWatson.py
+++ mlPredictorWatson.py
@@ -1,10 +1,18 @@
 import requests
-from mlHelperFuncts import transform, inverse
-
+from mlHelperFuncts import transform, inverse #For normalization
 import urllib3, requests, json
 
-
+#More on Normalization : https://www.analyticsvidhya.com/blog/2020/04/feature-scaling-machine-learning-normalization-standardization/
+#Normalization of data is done as ML Models perform better on Normalizaed data
 def predictPower(windspeed, winddirection):
+    """ 
+    Parameters:
+        windspeed -> list of windspeeds m/s
+        winddirection ->list of wind directions degree
+    Return power in KW
+    This funtion first normalizes the values of windspeed and winddirections then passes it to APIWrapper and then 
+    denormalizes the result back and returns it
+    """
     api_input = []
     for i in range(len(windspeed)):
         api_input.append(transform(windspeed[i], winddirection[i]))
@@ -17,7 +25,13 @@ def predictPower(windspeed, winddirection):
 
 def predictorAPIWrapper(api_input):
     
-    # print(api_input)
+    """
+    Parameters:
+        api_input = list of list of windspeed(normalized) and winddirection(normalized)
+        Return a list of normalized power outputs 
+
+    """
+
     # NOTE: generate iam_token and retrieve ml_instance_id based on provided documentation	
     iam_token = ''
     with open('iamtoken.txt', 'r') as f:
@@ -34,7 +48,7 @@ def predictorAPIWrapper(api_input):
     try:
         power_pred = response_scoring.json()['predictions'][0]['values']
     except :
-        print(f"Error in the IBM AI service RESPONCE : {response_scoring.json()}")
+        raise Exception(f"Error in the IBM AI service RESPONCE ---> : {response_scoring.json()}")
 
     return power_pred
    
