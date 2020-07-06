63c7a05bc937e2ae2675dd4a6a45b10759c086f8
anantdevcs <anantdeviitism@gmail.com>
2020-07-06 23:59:46 +0530

    removeed files from git

--- download.py
+++
@@ -1,7 +0,0 @@
-
-# https://drive.google.com/file/d/1K2obxXAwzg63KlRAlBkuIxB79YCYq9lP/view?usp=sharing
-from google_drive_downloader import GoogleDriveDownloader as gdd
-
-gdd.download_file_from_google_drive(file_id='1K2obxXAwzg63KlRAlBkuIxB79YCYq9lP',
-                                    dest_path='./data',
-                                    )
